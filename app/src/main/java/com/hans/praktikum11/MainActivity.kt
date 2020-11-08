58de804c88981dc38698cbcc1437b1d494818d26
hafzansh <hafizansh.m@gmail.com>
2020-11-08 11:35:29 +0800

    remove app fragment -> added materi minggu 2

--- app/src/main/java/com/hans/praktikum11/MainActivity.kt
+++ app/src/main/java/com/hans/praktikum11/MainActivity.kt
@@ -5,6 +5,9 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,27 +16,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        val iEmail = findViewById<EditText>(R.id.textMail)
+        val iPassword = findViewById<EditText>(R.id.textPassword)
+        val Login = findViewById<Button>(R.id.loginBt)
+
+        loginBt.setOnClickListener{
+            val Email = iEmail.text.toString().trim()
+            val Password = iPassword.text.toString()
+            Toast.makeText(this,Email, Toast.LENGTH_SHORT).show()
+
         }
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
     }
 }
