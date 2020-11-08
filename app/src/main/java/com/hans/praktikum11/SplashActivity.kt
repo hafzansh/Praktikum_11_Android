de9589f12711b1c50ab128bc5f07ffcfa3b79fed
hafzansh <hafizansh.m@gmail.com>
2020-11-08 12:41:24 +0800

    splash screen added

---
+++ app/src/main/java/com/hans/praktikum11/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.hans.praktikum11
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed( {
+            val i = Intent(this@SplashActivity,MainActivity::class.java)
+            startActivity(i)
+            finish()
+        },2000)
+    }
+}
